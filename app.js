c85b7035422011eebfcf92b65c11874172c928eb
hameedullah <vjhameed3@gmail.com>
2017-05-06 11:23:16 -0700

    included comments

--- app.js
+++ app.js
@@ -4,13 +4,17 @@
 const express = require('express');
 var app = express();
 
-
+//a simple express web server
 app.set("view engine","ejs");
 app.use(express.static("/public"));
 
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.url}`)
+    next ()
+})
 
 app.get("/",(req,res)=>{
         res.render("index");
-});
+})
 
 app.listen(3000,()=>console.log("server has started"));
